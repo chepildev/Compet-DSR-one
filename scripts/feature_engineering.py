95a56de189aa257f9be27adcacec4fa82e321baf
tautme <hughes@pobox.com>
2023-05-05 10:35:13 +0200

    move date column drop

--- scripts/feature_engineering.py
+++ scripts/feature_engineering.py
@@ -2,13 +2,17 @@
 
 def feature_eng(df):
     # drop week start date column
-    df.drop('week_start_date', axis=1, inplace=True)
+    # df.drop('week_start_date', axis=1, inplace=True)
     # fill na features with ffills
     df.fillna(method='ffill', inplace=True)
     # select features we want
     features = ['reanalysis_specific_humidity_g_per_kg', 
                  'reanalysis_dew_point_temp_k', 
                  'station_avg_temp_c', 
-                 'station_min_temp_c']
+                 'station_min_temp_c',
+                 'total_cases']    
     df = df[features]
+    # add jitter to station_min_temp_c
+    df['station_min_temp_c'] = df['station_min_temp_c'] + np.random.uniform(-0.234332, 0.251324, len(df))
+
     return df
