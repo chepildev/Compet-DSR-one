3fbe2f3c58eef2d8b34c88aca5728b3d1b3fcd62
Paul Jewell <pauldavidjewell@gmail.com>
2023-05-06 04:19:39 +0200

    updated

--- scripts/final_predict.py
+++ scripts/final_predict.py
@@ -36,6 +36,31 @@ def final_predict(X_test, X_train, y_train :pd.DataFrame,
     return final_preds
 
 
+def preds_for_plot(X_test, X_train, y_train :pd.DataFrame, model:str, params={}) -> pd.DataFrame:
+    """_summary_
+    Args:
+        final_test (_type_): _description_
+        X_train (_type_): _description_
+        y_train (pd.DataFrame): _description_
+        city (str): _description_
+        model (str): _description_
+        params (dict, optional): _description_. Defaults to {}.
+    Returns:
+        pd.DataFrame: _description_
+    """    
+    scaler = MinMaxScaler()
+    scaler.fit(X_train)
+    X_train_scaled = scaler.transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    m = eval(model + "(**params)")
+    m.fit(X_train_scaled, y_train)
+    preds_train = m.predict(X_train_scaled)
+
+    return preds_train, y_train
+
+
+
 def write_submission(final_iq: pd.DataFrame, final_sj: pd.DataFrame) -> pd.DataFrame:
     """_summary_
 
